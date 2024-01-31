7c3089b3905e07f1ab98a51484115fe88086cf74
pyk <gm@pyk.sh>
2024-01-31 09:40:43 +0700

    chore: initialize L1Escrow

---
+++ src/L1Escrow.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+/**
+ * @title L1Escrow
+ * @author sepyke.eth
+ * @dev This contract is what keeps the L2Token backed up on the origin chain
+ */
+contract L1Escrow {
+  constructor () {
+  }
+}
